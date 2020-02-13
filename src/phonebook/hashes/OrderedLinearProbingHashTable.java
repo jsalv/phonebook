5c275ef8e43bcf6a888f430523146385e5858897
Michael Marsh <mmarsh@cs.umd.edu>
2020-02-13 09:50:31 -0500

    initial import

---
+++ src/phonebook/hashes/OrderedLinearProbingHashTable.java
@@ -0,0 +1,107 @@
+package phonebook.hashes;
+
+import phonebook.exceptions.UnimplementedMethodException;
+import phonebook.utils.PrimeGenerator;
+import phonebook.utils.Probes;
+
+/**
+ * <p>{@link OrderedLinearProbingHashTable} is an Openly Addressed {@link HashTable} implemented with
+ * <b>Ordered Linear Probing</b> as its collision resolution strategy: every key collision is resolved by moving
+ * one address over, and the keys in the chain is in order. It suffer from the &quot; clustering &quot; problem:
+ * collision resolutions tend to cluster collision chains locally, making it hard for new keys to be
+ * inserted without collisions. {@link QuadraticProbingHashTable} is a {@link HashTable} that
+ * tries to avoid this problem, albeit sacrificing cache locality.</p>
+ *
+ * @author YOUR NAME HERE!
+ *
+ * @see HashTable
+ * @see SeparateChainingHashTable
+ * @see LinearProbingHashTable
+ * @see QuadraticProbingHashTable
+ * @see CollisionResolver
+ */
+public class OrderedLinearProbingHashTable extends OpenAddressingHashTable {
+
+    /* ********************************************************************/
+    /* ** INSERT ANY PRIVATE METHODS OR FIELDS YOU WANT TO USE HERE: ******/
+    /* ********************************************************************/
+
+    /* ******************************************/
+    /*  IMPLEMENT THE FOLLOWING PUBLIC METHODS: */
+    /* **************************************** */
+
+    /**
+     * Constructor with soft deletion option. Initializes the internal storage with a size equal to the starting value of  {@link PrimeGenerator}.
+     * @param soft A boolean indicator of whether we want to use soft deletion or not. {@code true} if and only if
+     *               we want soft deletion, {@code false} otherwise.
+     */
+    public OrderedLinearProbingHashTable(boolean soft){
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER IMPLEMENTING THIS METHOD!
+    }
+
+
+
+    /**
+     * Inserts the pair &lt;key, value&gt; into this. The container should <b>not</b> allow for {@code null}
+     * keys and values, and we <b>will</b> test if you are throwing a {@link IllegalArgumentException} from your code
+     * if this method is given {@code null} arguments! It is important that we establish that no {@code null} entries
+     * can exist in our database because the semantics of {@link #get(String)} and {@link #remove(String)} are that they
+     * return {@code null} if, and only if, their key parameter is {@code null}. This method is expected to run in <em>amortized
+     * constant time</em>.
+     *
+     * Different from {@link LinearProbingHashTable}, the keys in the chain are <b>in order</b>. As a result, we might increase
+     * the cost of insertion and reduce the cost on search miss. One thing to notice is that, in soft deletion, we ignore
+     * the tombstone during the reordering of the keys in the chain. We will have some example in the writeup.
+     *
+     * Instances of {@link OrderedLinearProbingHashTable} will follow the writeup's guidelines about how to internally resize
+     * the hash table when the capacity exceeds 50&#37;
+     * @param key The record's key.
+     * @param value The record's value.
+     * @throws IllegalArgumentException if either argument is {@code null}.
+     * @return The {@link phonebook.utils.Probes} with the value added and the number of probes it makes.
+     */
+    @Override
+    public Probes put(String key, String value) {
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER IMPLEMENTING THIS METHOD!
+    }
+
+    @Override
+    public Probes get(String key) {
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER IMPLEMENTING THIS METHOD!
+    }
+
+
+    /**
+     * <b>Return</b> and <b>remove</b> the value associated with key in the {@link HashTable}. If key does not exist in the database
+     * or if key = {@code null}, this method returns {@code null}. This method is expected to run in <em>amortized constant time</em>.
+     *
+     * @param key The key to search for.
+     * @return The {@link phonebook.utils.Probes} with associated value and the number of probe used. If the key is {@code null}, return value {@code null}
+     * and 0 as number of probes; if the key dones't exists in the database, return {@code null} and the number of probes used.
+     */
+    @Override
+    public Probes remove(String key) {
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER IMPLEMENTING THIS METHOD!
+    }
+
+    @Override
+    public boolean containsKey(String key) {
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER IMPLEMENTING THIS METHOD!
+    }
+
+    @Override
+    public boolean containsValue(String value) {
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER IMPLEMENTING THIS METHOD!
+    }
+
+    @Override
+    public int size() {
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER IMPLEMENTING THIS METHOD!
+    }
+
+    @Override
+    public int capacity() {
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER IMPLEMENTING THIS METHOD!
+    }
+
+}
